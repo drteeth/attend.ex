fcaac4597143b71549b860171f986cd91e3111b6
Benjamin Moss <drteeth@gmail.com>
2017-03-14 12:22:27 -0400

    check for dispatch success

--- apps/domain/test/domain/user_test.exs
+++ apps/domain/test/domain/user_test.exs
@@ -11,17 +11,17 @@ defmodule Attend.UserTest do
 
   test "register user command" do
     register_user_command = RegisterUser.register(name: "Ben Moss", email: "drteeth@gmail.com")
-    Router.dispatch(register_user_command)
+    :ok = Router.dispatch(register_user_command)
 
     register_team_command = RegisterTeam.register(name: "The Penguins")
-    Router.dispatch(register_team_command)
+    :ok = Router.dispatch(register_team_command)
 
     join_team_command = %JoinTeam{
       user_id: register_user_command.id,
       team_id: register_team_command.id,
     }
 
-    Router.dispatch(join_team_command)
+    :ok = Router.dispatch(join_team_command)
 
     {:ok, events} = EventStore.read_stream_forward(register_team_command.id)
     team = List.foldl(events, %Team{}, fn (event, team) ->
